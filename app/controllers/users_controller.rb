d2d3aedd094faf6307a81743614e9b8fe1331849
Akumalla Mohammad Waris <wari@Wariss-MacBook-Air.local>
2026-01-23 12:24:47 +0530

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user,   only: %i[edit update]
   before_action :admin_user,     only: :destroy
 
   def index
@@ -19,10 +19,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render "new", status: :unprocessable_entity
     end
@@ -71,12 +70,12 @@ class UsersController < ApplicationController
 
   # Confirms an admin user.
   def admin_user
-    redirect_to(root_url, status: :see_other) unless current_user.admin?
+    redirect_to root_url, status: :see_other unless current_user.admin?
   end
 
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    redirect_to root_url, status: :see_other unless current_user?(@user)
   end
 end
---
+++ test/controllers/account_activations_controller_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
+  def edit
+
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
